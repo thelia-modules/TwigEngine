c6fef7b2355640a3fb909770bbe39b392adf5726
lnormandon <lnormandon@openstudio.fr>
2025-01-22 11:39:16 +0100

    remove unsused function getProvider

--- Service/API/MetadataService.php
+++ Service/API/MetadataService.php
@@ -12,30 +12,24 @@
 
 namespace TwigEngine\Service\API;
 
-use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Exception\ResourceClassNotFoundException;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
 use ApiPlatform\Metadata\ResourceAccessCheckerInterface;
-use ApiPlatform\State\ProviderInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\AccessMapInterface;
-use Thelia\Api\Bridge\Propel\State\PropelCollectionProvider;
-use Thelia\Api\Bridge\Propel\State\PropelItemProvider;
 use Thelia\Core\Security\SecurityContext;
 
 readonly class MetadataService
 {
     public function __construct(
         private ResourceMetadataCollectionFactoryInterface $resourceMetadataCollectionFactory,
-        private PropelCollectionProvider                   $propelCollectionProvider,
-        private PropelItemProvider                         $propelItemProvider,
         #[Autowire(service: 'security.access_map')]
-        private AccessMapInterface                         $accessMap,
-        private SecurityContext                            $securityContext,
+        private AccessMapInterface $accessMap,
+        private SecurityContext $securityContext,
         #[Autowire(service: 'api_platform.security.resource_access_checker')]
-        private ResourceAccessCheckerInterface             $resourceAccessChecker,
+        private ResourceAccessCheckerInterface $resourceAccessChecker,
     ) {
     }
 
@@ -59,15 +53,6 @@ readonly class MetadataService
         return null;
     }
 
-    public function getProvider(Operation $operation): ProviderInterface
-    {
-        if ($operation instanceof CollectionOperationInterface) {
-            return $this->propelCollectionProvider;
-        }
-
-        return $this->propelItemProvider;
-    }
-
     public function canUserAccessResource(
         string $resourceClass,
         string $path,
@@ -81,14 +66,15 @@ readonly class MetadataService
             throw new \LogicException('Cannot check security expression when SecurityBundle is not installed. Try running "composer require symfony/security-bundle".');
         }
         if ($isGranted !== null) {
-            return $this->resourceAccessChecker->isGranted($resourceClass, $isGranted, $context['extra_variables'] ?? []);
+            return $this->resourceAccessChecker->isGranted($resourceClass, $isGranted,
+                $context['extra_variables'] ?? []);
         }
         $request = Request::create($path, $method);
         [$roles, $channel] = $this->accessMap->getPatterns($request);
         if (null === $roles) {
             return true;
         }
-        $user = $this->isAdminRoute($path) ? $this->securityContext->getAdminUser() : $this->securityContext->getCustomerUser();
+        $user      = $this->isAdminRoute($path) ? $this->securityContext->getAdminUser() : $this->securityContext->getCustomerUser();
         $userRoles = $user ? $user->getRoles() : [];
 
         if (!empty(array_intersect($userRoles, $roles))) {
